d8321cb62610b068bb0f3318a6021b94101307a5
Miha Rihtaršič <miha@kamnitnik.top>
2021-10-18 14:48:19 +0200

    Version 1.2

--- coterm.el
+++ coterm.el
@@ -4,7 +4,7 @@
 
 ;; Filename: coterm.el
 ;; Author: jakanakaevangeli <jakanakaevangeli@chiru.no>
-;; Version: 1.1
+;; Version: 1.2
 ;; Keywords: processes
 ;; Package-Requires: ((emacs "26.1"))
 ;; URL: https://repo.or.cz/emacs-coterm.git
